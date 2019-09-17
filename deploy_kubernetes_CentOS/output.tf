e40ce9ba08c7ef22df172f726d485670d0e0127d
Anthony Spiteri <anthony@sliema.com.au>
2019-09-17 16:26:04 +0800

    Update output.tf

--- deploy_kubernetes_CentOS/output.tf
+++ deploy_kubernetes_CentOS/output.tf
@@ -4,15 +4,3 @@ Output Variables
 output "vsphere_ipv4_address" {
   value = "${var.vsphere_ipv4_address}"
 }
-
-output "vsphere_ipv4_address_k8n1" {
-  value = "${var.vsphere_ipv4_address_k8n1}"
-}
-
-output "vsphere_ipv4_address_k8n2" {
-  value = "${var.vsphere_ipv4_address_k8n2}"
-}
-
-output "vsphere_ipv4_address_k8n3" {
-  value = "${var.vsphere_ipv4_address_k8n3}"
-}
