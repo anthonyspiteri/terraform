d13fed18dcfca77b90283f7141914e79838afd52
Anthony Spiteri <anthony@sliema.com.au>
2020-03-05 22:34:58 +0800

    Update choco_veeam.ps1

--- choco_veeam.ps1
+++ choco_veeam.ps1
@@ -3,5 +3,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco feature enable -n=exitOnRebootDetected
 choco feature enable -n=useRememberedArgumentsForUpgrades
 
-choco install sql-server-express
-#choco install veeam-backup-and-replication-server -y"
+choco install sql-server-express -y
+
+choco install veeam-backup-and-replication-server --params "/sqlServer:(local)\SQLEXPRESS /licenseFile:c:\packages\license-preview-1000-15FEB2020.lic /username:autodeploy /password:Veeam1!" --source='"c:\Packages;chocolatey"'
